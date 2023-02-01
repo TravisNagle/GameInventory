e3204d94305e223769bc2e6ccc8c19b6927c0824
iStratix <itsnagle@gmail.com>
2023-02-01 18:01:28 -0500

    Fixed issue where Weapon inherited from Item instead of Clue

--- GameInventory/Program.cs
+++ GameInventory/Program.cs
@@ -6,7 +6,7 @@
         {
             Item item = new Item("Gaming", "Gamer", RarityType.COMMON, 1.5, 105.3);
             Item item2 = new Item("Rare type item", "Thing", RarityType.RARE, 200, 1054);
-            Weapon weapon = new Weapon("Its big", "Big Axe", RarityType.UNCOMMON, 400, 205.5, 50, DamageType.SLASHING);
+            Weapon weapon = new Weapon("Its big", "Big Axe", RarityType.UNCOMMON, 400, 205.5, 50, DamageType.SLASHING, true, "Mrs. White");
             Clue clue = new Clue("its a clue", "Paper", RarityType.COMMON, 10, 1, false, "Colonel Mustard");
             BackPack pack = new BackPack();
 
--- GameInventory/Weapon.cs
+++ GameInventory/Weapon.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 
 namespace GameInventory
 {
-    internal class Weapon : Item
+    internal class Weapon : Clue
     {
         protected int Damage { get; set; }
         protected DamageType DamageType { get; set; }
 
-        public Weapon(string description, string name, RarityType rarity, double value, double weight, int damage, DamageType damageType) : base(description, name, rarity, value, weight)
+        public Weapon(string description, string name, RarityType rarity, double value, double weight, int damage, 
+            DamageType damageType, bool accuracy, string incriminated) : base(description, name, rarity, value, weight, accuracy, incriminated)
         {
             Damage = damage;
             DamageType = damageType;
