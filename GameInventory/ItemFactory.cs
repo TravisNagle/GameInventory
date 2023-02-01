d5276a3cdff890e2061c3e9d0ef4c2c78e40cd77
iStratix <itsnagle@gmail.com>
2023-02-01 17:36:49 -0500

    Created foundations for MakeRandomClue, MakeRandomClue overload, MakeRandomItem, MakeRandomWeapon, and MakeRandomWeapon overload methods for ItemFactory.

--- GameInventory/ItemFactory.cs
+++ GameInventory/ItemFactory.cs
@@ -8,5 +8,29 @@ namespace GameInventory
 {
     internal class ItemFactory
     {
+        public Clue MakeRandomClue()
+        {
+            return null;
+        }
+
+        public Clue MakeRandomClue(bool isAccurate, string inciminate)
+        {
+            return null;
+        }
+
+        public Item MakeRandomItem()
+        {
+            return null;
+        }
+
+        public Weapon MakeRandomWeapon(RarityType rarity)
+        {
+            return null;
+        }
+
+        public Weapon MakeRandomWeapon(RarityType rarity, bool isAccurate, string incriminate)
+        {
+            return null;
+        }
     }
 }
