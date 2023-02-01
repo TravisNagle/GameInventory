ba06bbf796b59ea7fb606434ba743b5af7256ce5
iStratix <itsnagle@gmail.com>
2023-02-01 17:46:32 -0500

    Weapon ToString test code

--- GameInventory/Program.cs
+++ GameInventory/Program.cs
@@ -6,9 +6,11 @@
         {
             Item item = new Item("Gaming", "Gamer", RarityType.COMMON, 1.5, 105.3);
             Item item2 = new Item("Rare type item", "Thing", RarityType.RARE, 200, 1054);
+            Weapon weapon = new Weapon("Its big", "Big Axe", RarityType.UNCOMMON, 400, 205.5, 50, DamageType.SLASHING);
 
             Console.WriteLine(item);
             Console.WriteLine(item2);
+            Console.WriteLine(weapon);
         }
     }
 }
