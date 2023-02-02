9686bad277a7664934f138d98611d1915d71628b
iStratix <itsnagle@gmail.com>
2023-02-01 19:46:08 -0500

    Implemented and tested MakeRandomClue and paramterized MakeRandomClue

--- GameInventory/ItemFactory.cs
+++ GameInventory/ItemFactory.cs
@@ -24,12 +24,61 @@ namespace GameInventory
 
             Clue clue = new Clue("", "", RarityType.COMMON, -1, -1, false, "");
 
+            switch(randomValue)
+            {
+                case 0:
+                    clue = new Clue("A conspicuous note", "Note", RarityType.COMMON, 1.50, 0.5, false, "Colonel Mustard");
+                    break;
+                case 1:
+                    clue = new Clue("A sprinkling of ash", "Ash", RarityType.COMMON, 0, 0.1, true, "Mrs. Peacock");
+                    break;
+                case 2:
+                    clue = new Clue("A recently snuffed candle", "Candle", RarityType.COMMON, 1.00, 0.2, true, "Mr. Green");
+                    break;
+                case 3:
+                    clue = new Clue("A cracked picture frame", "Framed Picture", RarityType.COMMON, 3.00, 1.0, false, "Mrs. White");
+                    break;
+                case 4:
+                    clue = new Clue("A threatening note", "Letter", RarityType.COMMON, 0.2, 0.1, false, "Miss Scarlet");
+                    break;
+                case 5:
+                    clue = new Clue("A small, red handkerchief", "Handkerchief", RarityType.COMMON, 0.5, 0.1, true, "Dr. Orchid");
+                    break;
+            }
+
             return clue;
         }
 
-        public Clue MakeRandomClue(bool isAccurate, string inciminate)
+        public Clue MakeRandomClue(bool isAccurate, string incriminate)
         {
-            return null;
+            Random rand = new Random();
+            int randomValue = rand.Next(0, 5);
+
+            Clue clue = new Clue("", "", RarityType.COMMON, -1, -1, false, "");
+
+            switch (randomValue)
+            {
+                case 0:
+                    clue = new Clue("A conspicuous note", "Note", RarityType.COMMON, 1.50, 0.5, isAccurate, incriminate);
+                    break;
+                case 1:
+                    clue = new Clue("A sprinkling of ash", "Ash", RarityType.COMMON, 0, 0.1, isAccurate, incriminate);
+                    break;
+                case 2:
+                    clue = new Clue("A recently snuffed candle", "Candle", RarityType.COMMON, 1.00, 0.2, isAccurate, incriminate);
+                    break;
+                case 3:
+                    clue = new Clue("A cracked picture frame", "Framed Picture", RarityType.COMMON, 3.00, 1.0, isAccurate, incriminate);
+                    break;
+                case 4:
+                    clue = new Clue("A threatening note", "Letter", RarityType.COMMON, 0.2, 0.1, isAccurate, incriminate);
+                    break;
+                case 5:
+                    clue = new Clue("A small, red handkerchief", "Handkerchief", RarityType.COMMON, 0.5, 0.1, isAccurate, incriminate);
+                    break;
+            }
+
+            return clue;
         }
 
         public Item MakeRandomItem()
--- GameInventory/Program.cs
+++ GameInventory/Program.cs
@@ -9,6 +9,9 @@
             Weapon weapon = new Weapon("Its big", "Big Axe", RarityType.UNCOMMON, 400, 205.5, 50, DamageType.SLASHING, true, "Mrs. White");
             Clue clue = new Clue("its a clue", "Paper", RarityType.COMMON, 10, 1, false, "Colonel Mustard");
             BackPack pack = new BackPack();
+            ItemFactory factory = new ItemFactory();
+            pack.Add(factory.MakeRandomClue());
+
 
             pack.Add(item);
             pack.Add(item2);
@@ -18,8 +21,8 @@
             //Console.WriteLine(item);
             //Console.WriteLine(item2);
             //Console.WriteLine(weapon);
-            Console.WriteLine(pack.ListWeapons());
-            Console.WriteLine(pack.ListClues());
+            //Console.WriteLine(pack.ListWeapons());
+            //Console.WriteLine(pack.ListClues());
 
             Console.WriteLine("---------------------------------------");
             Console.WriteLine(pack.ListItems());
