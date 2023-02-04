41aaba160b2a8ecb4bcbd1f97bded433789ed25e
iStratix <itsnagle@gmail.com>
2023-02-04 14:01:10 -0500

    XML added to Weapon class

--- GameInventory/Weapon.cs
+++ GameInventory/Weapon.cs
@@ -15,11 +15,28 @@ using System.Threading.Tasks;
 
 namespace GameInventory
 {
+    /// <summary>
+    /// Implementation of the Weapon class that addes damage and damage type
+    /// to the Item/Clue parent class
+    /// </summary>
     internal class Weapon : Clue
     {
         protected int Damage { get; set; }
         protected DamageType DamageType { get; set; }
 
+        /// <summary>
+        /// Parameterized constructor that allows for the input of damage and damage
+        /// type values to separate from the Clue objects in the BackPack lists
+        /// </summary>
+        /// <param name="description">Description of the weapon</param>
+        /// <param name="name">Name of the weapon</param>
+        /// <param name="rarity">Rarity value of the weapon</param>
+        /// <param name="value">Monetary value of the weapon</param>
+        /// <param name="weight">Weight of the weapon</param>
+        /// <param name="damage">The number of hit points the weapon deals</param>
+        /// <param name="damageType">What type of damage the weapon deals</param>
+        /// <param name="accuracy">If the weapon is an accurate clue</param>
+        /// <param name="incriminated">Which person this weapon incriminates</param>
         public Weapon(string description, string name, RarityType rarity, double value, double weight, int damage, 
             DamageType damageType, bool accuracy, string incriminated) : base(description, name, rarity, value, weight, accuracy, incriminated)
         {
@@ -27,6 +44,11 @@ namespace GameInventory
             DamageType = damageType;
         }
 
+        /// <summary>
+        /// Converts Weapon to a string with DamageType and Damage
+        /// values listed vertically
+        /// </summary>
+        /// <returns>Weapon as a string</returns>
         public override string ToString()
         {
             string info = "";
